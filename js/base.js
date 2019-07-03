32df5c27b283b1bff6b23785d359c8fab523ef77
gusthavosouza <gusthavo.souzapereira@gmail.com>
2019-07-03 14:19:49 -0300

    update cloudfrond

---
+++ js/base.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+	var scroll = $(window).scrollTop();
+
+	if (scroll >= 250) {
+		$(".navbar").addClass("menu-normal");
+	} else {
+		$(".navbar").removeClass("menu-normal");
+	}
+
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 250) {
+			$(".navbar").addClass("menu-normal");
+		} else {
+			$(".navbar").removeClass("menu-normal");
+		}
+	});
+};
+
+function smoothScroll(id){document.getElementById(id).scrollIntoView({block:'start',behavior:'smooth'})}
